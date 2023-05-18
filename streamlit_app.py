8bfd0d7928cb0839a27aca3bba2cd1a6ac46f5d2
cherryg14 <133969236+cherryg14@users.noreply.github.com>
2023-05-18 17:16:12 +0530

    Update streamlit_app.py
    adding extra lines for testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
 streamlit.title('My parents new healthy dinner') 
+streamlit.header('Breakfast menu')
+streamlit.text('blueberry & oat meal')
+streamlit.text('Kale, spinach smoothie')
+streamlit.text('Hard boiled egg')
+               
+               
